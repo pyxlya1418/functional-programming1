feec8ea8fee4706b42f8fbd2ddf5739abd44eed6
pyxlya1418 <153579327+pyxlya1418@users.noreply.github.com>
2023-12-14 14:30:45 +0200

    Update increase_by_ten.py.py

--- increase_by_ten.py.py
+++ increase_by_ten.py.py
@@ -1,2 +1,15 @@
 def increase_by_ten(numbers):
     return [number + 10 for number in numbers]
+    
+import unittest
+
+class IncreaseByTenTest(unittest.TestCase):
+
+    def test_increase_by_ten(self):
+        numbers = [1, 2, 3, 4, 5]
+        expected_results = [11, 12, 13, 14, 15]
+        actual_results = increase_by_ten(numbers)
+        self.assertEqual(actual_results, expected_results)
+
+if __name__ == "__main__":
+    unittest.main()
